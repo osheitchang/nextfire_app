e5f7816cb55fc49871eff9bd299dcc04dcb66217
osheit chang <chang.osheit@gmail.com>
2023-04-10 18:41:42 -0400

    navbar & notifications added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from 'next/head'
-
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import Loader from '../components/Loader';
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <Link href={{
+        pathname: '/[username]',
+        query: { username: 'Chang' }
+      }}>
+
+      </Link>
+      <div>
+        <Loader show />
+      </div>
+
+      <div>
+        <button onClick={() => toast.success("Thsi is a new message")} >
+          Press Me
+        </button>
+      </div>
+
     </div>
   )
 }
