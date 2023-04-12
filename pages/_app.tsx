2b22088e19ecf4d8afc69598b3c8ac4ebd801e7d
osheit chang <chang.osheit@gmail.com>
2023-04-12 19:57:53 -0400

    google sign in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
-import { UserContext } from '../lib/context'
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <UserContext.Provider value={{ user: {}, username: 'jeff'}}>
+      <UserContext.Provider value={useUserData}>
         <Navbar />
         <Component {...pageProps} />
         <Toaster />
